5b4e13c779a6c352cb93042e317d9c9860fcfaaa
RexZhann <yz9621@ic.ac.uk>
2024-08-15 15:14:03 +0800

    '

--- modules/main.py
+++ modules/main.py
@@ -15,15 +15,17 @@ import fitz
 from pysbd import Segmenter
 from tqdm import tqdm
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog
 from Ui_search import Ui_Dialog
-from modules import cluster_eval
+from modules import *
 
 
-def search_process(keyword, dep_key, corpora, modl='qwen-max'):
+def search_process(keyword, dep_key, corpus, api, modl='qwen-max'):
+    
     pass
 
-class MyMainWindow(QMainWindow,Ui_Dialog): #这里也要记得改
+class MyMainWindow(QMainWindow,Ui_Dialog): 
+
     def __init__(self,parent =None):
         super(MyMainWindow,self).__init__(parent)
         self.setupUi(self)
@@ -32,26 +34,36 @@ class MyMainWindow(QMainWindow,Ui_Dialog): #这里也要记得改
         self.searchButton.clicked.connect(self.on_search_clicked)
 
     def on_upload_clicked(self):
-        # 当上传按钮被点击时执行
-        # 获取lineEdit的文本
-        search_text = self.lineEdit.text()
-        # 获取comboBox_2的当前选中文本
+        file_path, _ = QFileDialog.getOpenFileName(self,
+                                                    "上传文件",  # 对话框标题
+                                                    "",  # 起始目录，默认打开的文件夹路径
+                                                    "All Files (*);;Text Files (*.txt)")  # 文件过滤器
         
+        # 检查用户是否选择了文件
+        if file_path:  # 如果用户没有取消操作，file_path将不是空的
+            # 在这里处理文件，例如获取文件路径
+            print(f"Selected file: {file_path}")
+
+            # 可以在这里添加代码来处理上传的文件
+            # 例如，使用Python的内置open函数来读取文件内容
+            # with open(file_path, 'r') as file:
+            #     content = file.read()
+            #     print(content)
         
-        # 执行上传操作...
-        print(f"Uploading with search text: {search_text}")
 
     def on_search_clicked(self):
-        # 当搜索按钮被点击时执行
-        # 获取lineEdit的文本
+
+
         keyword = self.keyword.text()
-        # 获取comboBox的当前选中文本
-        
-        selected_corpus = self.currentCorpora.items()
+        corpus = self.currentCorpora.items()
         dep_key = self.dep_key.currentText()
+        # 临时placeholder变量
+        api = [0, 0, 0]
+        modl = 'qwen-max'
         
-        # 执行搜索操作...
-        print(f"Searching for: {keyword} in corpus: {selected_corpus}")
+        search_process(keyword, dep_key, corpus, api, modl)
+
+        print(f"Searching for: {keyword} in corpus: {corpus}")
 
 
 
